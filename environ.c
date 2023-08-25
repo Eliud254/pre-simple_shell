f7e407b8aae554da751859970009edd088e3750c
josephmarko <josephmmakaro@gmail.com>
2023-08-25 18:57:35 +0000

    Execute function

---
+++ environ.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+char *_getenv(char *_env)
+{
+    int checker, i, j;
+
+Here:
+    if (environ[i])
+    {
+        checker = 1;
+
+Again:
+        if (environ[i][j] != '=')
+        {
+            if(environ[i][j] != _env[j])
+                checker = 0;
+            j++;
+            goto Again;
+        }
+
+        while (checker == 1)
+            break;
+
+        goto Here;
+    }
+
+    return (&environ[i][j + 1]);
+}
+
+void _env(void)
+{
+    int i = 0;
+
+Here:
+    if (environ[i])
+        printf("%s\n", environ[i]);
+        i++;
+        goto Here;
+}
--- shell.h
+++ shell.h
@@ -1,20 +1,11 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H
+#define _SHELL_H
 
-/* == Global Variables == */
-extern char **environ;
-
-/* == Standard Libraries == */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-
-/* == Struct Varaibles == */
-
-/* == Prototype functions */
-char _strlen(char *str);
 
 #endif
