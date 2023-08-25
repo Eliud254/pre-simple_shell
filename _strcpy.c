050a9ed90b0f75091ef730bcbca5c9807ca9c9e3
josephmarko <josephmmakaro@gmail.com>
2023-08-25 17:26:35 +0000

    Done strcpy Function

---
+++ _strcpy.c
@@ -0,0 +1,54 @@
+#include <string.h>
+#include <stdio.h>
+
+/* ================    Strcpy Functions ==================== */
+
+char* _strcpy(char* dest, const char* src)
+{
+    char* str;
+
+    str = dest;
+
+Here:
+    if (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+
+        goto Here;
+    }
+
+    *dest = '\0';
+
+    return (str);
+}
+
+/* ========================= Dont Copy this side ====================== */
+
+int main()
+{
+    char src[] = "Source File";
+    char dest[100];
+
+    char *result1 = strcpy(dest, src);
+    char *result2 = _strcpy(dest,src);
+    
+    putchar('\n');
+    
+    printf("|=== Actual ===|\n");
+    putchar('\n');
+    
+    printf("Src = %s\n", src);
+    printf("=============================\n");
+    printf("Actual = %s\n", result1);
+    putchar('\n');
+    
+    printf("|=== Test ===|\n");
+    putchar('\n');
+
+    printf("Src = %s\n", src);
+    printf("=============================\n");
+    printf("Customized = %s\n", result2);
+    putchar('\n');
+}
